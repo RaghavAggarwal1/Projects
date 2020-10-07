5fe985a99c1b480c8c1aabb548b1d2eddeb0d0f6
RaghavAggarwal1 <43023682+RaghavAggarwal1@users.noreply.github.com>
2020-10-07 12:50:57 -0700

    Add files via upload

---
+++ CSE 205/Assignment 3/Assignment 3/Assignment3.java
@@ -0,0 +1,46 @@
+
+/*
+Assignment #: 3
+Arizona State University - CSE205
+        Name: Raghav Aggarwal
+   StudentID: 1215935292
+     Lecture: MW 4:35pm - 5:50pm
+
+*/
+/*
+
+
+1. CLIENT AND EMPLOYER - Software engineers shall act in a manner that is in the best interests of their client and employer consistent with the public interest. 
+
+In my words -> Software engineers should be honest and transparent with thir clients or employers. and most importantly they should always do work which is alligns with public interest and doesnt harm humanity.
+
+example -> if a software engineer doesnt know how to use a software which is required to be used for a project, but tells to the employer that he has experience with the software. But due to lack of experience he create a device which works but cannot compete the products in the market. Then the engineer is breaking the law.
+
+
+2. JUDGMENT - Software engineers shall maintain integrity and independence in their professional judgment. 
+
+In my words -> Software engineers should maintain professional Integrity  and objectivity. 
+
+example -> If a software engineer is taking Bribes from a client. This money is not on his contract. And this is an unethical practice as he could manupulate the information of his official client. Then the engineer is breaking the law.
+
+
+3. MANAGEMENT - Software engineering managers and leaders shall subscribe to and promote an ethical approach to the management of software development and maintenance. 
+
+In my words -> Software engineering managers and leaders should promote only ethical approach towards projects; including both promotion of quality and reduction of risks. And making sure that thier team undersatnads all the standards precisely.
+
+example -> If a team developed a software. And the head is not giving the specified portion of ownership to the software engineer (who has contributed) which was mentioned in the agreement. Then the engineer is breaking the law.
+
+
+4. PROFESSION -  Software engineers shall advance the integrity and reputation of the profession consistent with the public interest. 
+
+In my words -> Software engineers shall advance the integrity and reputation of their profession consistently with their public interest. 
+
+example-> If a software engineer made a software but didnt rectify any hidden errors or didnt correct or report them before making it available to the public. then it is the violation of this rule.
+
+
+5. COLLEAGUES - Software engineers shall be fair to and supportive of their colleagues. 
+
+in my words -> Software engineers shall be fair to and supportive of their colleagues. 
+
+example -> If sopftware engineer is not giving the required credits to his team and taking undue credits, then he is violating this rule. 
+*/
